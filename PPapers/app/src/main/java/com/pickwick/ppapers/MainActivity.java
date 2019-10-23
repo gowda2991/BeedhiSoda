33832912328d5eeddc014a1a70b50311d276d9a0
Kiran Karanth <kikarant@microsoft.com>
2019-10-23 22:05:25 +0530

    Bottom toolbar

--- PPapers/app/src/main/java/com/pickwick/ppapers/MainActivity.java
+++ PPapers/app/src/main/java/com/pickwick/ppapers/MainActivity.java
@@ -6,21 +6,28 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import androidx.core.widget.NestedScrollView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView navigation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.individual_excerpt_card_gradient);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        getSupportActionBar().setTitle(null);
+        navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setVisibility(View.GONE);
+        navigation.animate().translationY(navigation.getHeight() * 2).setStartDelay(0).setDuration(10).start();
+        //Toolbar toolbar = findViewById(R.id.toolbar);
+        //setSupportActionBar(toolbar);
+        //getSupportActionBar().setTitle(null);
         /*Toolbar toolbar = findViewById(R.id.toolbar);
 
 
@@ -32,6 +39,19 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });*/
+
+        NestedScrollView nested_content = (NestedScrollView) findViewById(R.id.nested_scroll_view);
+        nested_content.setOnScrollChangeListener(new NestedScrollView.OnScrollChangeListener() {
+            @Override
+            public void onScrollChange(NestedScrollView v, int scrollX, int scrollY, int oldScrollX, int oldScrollY) {
+                if (scrollY < oldScrollY) { // up
+                    animateNavigation(true);
+                }
+                if (scrollY > oldScrollY) { // down
+                    animateNavigation(false);
+                }
+            }
+        });
     }
 
     @Override
@@ -54,5 +74,17 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    boolean isNavigationHide = false;
+
+    private void animateNavigation(final boolean hide) {
+        int height = navigation.getHeight() * 2;
+        navigation.setVisibility(View.VISIBLE);
+        if (isNavigationHide && hide || !isNavigationHide && !hide) return;
+        isNavigationHide = hide;
+        int moveY = hide ? (2 * navigation.getHeight()) : 0;
+
+        navigation.animate().translationY(moveY).setStartDelay(0).setDuration(1000).start();
     }
 }
